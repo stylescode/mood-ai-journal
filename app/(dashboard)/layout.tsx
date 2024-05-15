dd19c1fd956cd7d8e7b3ae6169e387c0f96e491c
Styles <stylesk11@gmail.com>
2024-05-15 13:01:38 -0700

    dashboard layout skeleton

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,24 @@
+import { UserButton } from "@clerk/nextjs"
+
+const DashboardLayout = ({ children }) => {
+
+  return (
+    <div className="w-screen h-screen relative">
+      <div className="absolute top-0 left-0 h-full w-[200px] border-r border-black/10">
+        Mood
+      </div>
+      <div className="ml-[200px]">
+        <header className="h-[70px] border-b border-black/10">
+          <div className="h-full w-full px-6 flex items-center justify-end">
+            <UserButton />
+          </div>
+        </header>
+        <div>
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DashboardLayout
