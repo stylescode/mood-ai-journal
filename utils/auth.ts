68b33881411c6a099fa3b4b3c9fcf9573e6925d7
Styles <stylesk11@gmail.com>
2024-05-15 13:23:50 -0700

    remove options

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,15 +1,13 @@
-import { auth } from "@clerk/nextjs/dist/types/server";
+import { auth } from "@clerk/nextjs/server";
 import { prisma } from "./db";
 
-export const getUserByClerkId = async ({ includes = {}, select = {}}) => {
+export const getUserByClerkId = async () => {
   const { userId } = await auth();
 
   const user = await prisma.user.findUniqueOrThrow({
     where: {
       clerkId: userId,
     },
-    select,
-    includes
   })
 
   return user;
