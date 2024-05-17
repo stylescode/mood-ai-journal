c973b117b8eef0b9e9564a66e91994e7acb2cee7
Styles <stylesk11@gmail.com>
2024-05-17 13:00:17 -0700

    user can add new journal entry

---
+++ components/NewEntry.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { createNewEntry } from "@/utils/api";
+import { useRouter } from "next/navigation";
+
+const NewEntry = () => {
+  const router = useRouter();
+
+  const handleNewEntry = async () => {
+    const data = await createNewEntry();
+    router.push(`/journal/${data.data.id}`)
+  }
+
+  return (
+    <div className="bg-white cursor-pointer overflow-hidden rounded-md border shadow p-4 text-3xl" onClick={handleNewEntry}>
+      + New Entry
+    </div>
+  )
+}
+
+export default NewEntry;
