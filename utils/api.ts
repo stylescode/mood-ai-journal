a70047c93efaf128943a31a2a13b8d7502b5834c
Styles <stylesk11@gmail.com>
2024-05-28 14:30:23 -0700

    create entry by sending post req to api

--- utils/api.ts
+++ utils/api.ts
@@ -2,7 +2,13 @@ const createURL = (path: string) => {
   return window.location.origin + path;
 }
 
+export const createNewEntry = async () => {
+  const res = await fetch(new Request(createURL('/api/journal'), {
+    method: 'POST',
+  }))
 
-export const createNewEntry = () => {
-
+  if (res.ok) {
+    const data = await res.json();
+    return data.data;
+  }
 }
