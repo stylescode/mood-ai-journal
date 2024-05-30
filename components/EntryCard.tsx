f27e4692479cbcd23f1e3e7d4a404e85d92b0447
Styles <stylesk11@gmail.com>
2024-05-30 13:07:34 -0700

    add card styling

--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -2,14 +2,16 @@ const EntryCard = ({ entry }) => {
   const date = new Date(entry.createdAt).toDateString();
 
   return (
-    <div className="bg-white rounded-md overflow-hidden">
-      <div>
+    <div className="divide-y divide-gray-400 bg-white rounded-md overflow-hidden cursor-pointer border shadow p-4">
+      <div className="py-3">
         {date}
       </div>
-      <div>
-        content here
+      <div className="py-3">
+        {entry.content}
       </div>
-      {entry.id}
+      <div className="py-3">
+        {entry.id}
+      </div>
     </div>
   )
 }
