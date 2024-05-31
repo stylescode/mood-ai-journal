3aaae2adebeb2f1d3f63302a5c9b3513e5041227
Styles <stylesk11@gmail.com>
2024-05-30 21:32:51 -0700

    prevent type issue w/ prisma variable

--- utils/db.ts
+++ utils/db.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+declare global {
+  var prisma: PrismaClient | undefined;
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({log: ['query']});
+export const prisma = global.prisma ?? new PrismaClient({log: ['query']});
 
 if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
+  global.prisma = prisma;
 }
