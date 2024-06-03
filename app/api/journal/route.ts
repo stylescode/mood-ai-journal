c357df51ac5e1ad247d7b91809bfc790c907f4cd
Styles <stylesk11@gmail.com>
2024-06-03 15:30:26 -0700

    create initial ai analysis for new entry

---
+++ app/api/journal/route.ts
@@ -0,0 +1,32 @@
+import { getUserByClerkId } from "@/utils/auth";
+import { prisma } from "@/utils/db"
+import { revalidatePath } from "next/cache";
+import { NextResponse } from "next/server";
+import { analyze } from '@/utils/ai';
+
+export const POST = async () => {
+  const user = await getUserByClerkId();
+
+  const entry = await prisma.journalEntry.create({
+    data: {
+      userId: user.id,
+      content: 'Write whatever you feel like'
+    }
+  })
+
+  const analysis = await analyze(entry.content);
+
+  await prisma.analysis.create({
+    data: {
+      entryId: entry.id,
+      mood: analysis.mood,
+      summary: analysis.summary,
+      color: analysis.color,
+      soundtrack: analysis.music
+    }
+  })
+
+  revalidatePath('/journal');
+
+  return NextResponse.json({ data: entry })
+}
