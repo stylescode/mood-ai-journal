267aa52548baf16433b092f7ede0d0d9b982d017
Styles <stylesk11@gmail.com>
2024-06-10 15:43:05 -0700

    create response chain on entries; give ai insight

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,8 +1,12 @@
 import OpenAI from 'openai';
+import { Document } from 'langchain/document';
+import { loadQARefineChain } from 'langchain/chains';
+import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai';
+import { MemoryVectorStore } from 'langchain/vectorstores/memory';
 
 export const analyze = async (prompt: string) => {
-  const openai = new OpenAI();
-  const completion = await openai.chat.completions.create({
+  const model = new OpenAI();
+  const completion = await model.chat.completions.create({
     messages: [
       {
         role: 'system',
@@ -16,14 +20,13 @@ export const analyze = async (prompt: string) => {
     ],
     model: 'gpt-3.5-turbo',
     temperature: 0.1,
+    response_format: {
+      "type": "json_object"
+    }
   });
-
-  console.log('---------------------------------');
-  console.log('COMPLETION:', completion.choices[0].message.content);
 
   try {
     const response = JSON.parse(completion.choices[0].message.content);
-    console.log('RESPONSE:', response);
     return response;
   } catch (error) {
     console.log('ERROR:', error);
@@ -35,4 +38,35 @@ export const analyze = async (prompt: string) => {
     };
   }
 
+}
+
+export const giveInsight = async (question: string, entries) => {
+
+  const docs = entries.map((entry) => {
+    return new Document({
+      pageContent: entry.content,
+      metadata: {
+        id: entry.id,
+        createdAt: entry.createdAt,
+        updatedAt: entry.updatedAt
+      }
+   });
+  })
+
+  const model = new ChatOpenAI({
+    model: 'gpt-3.5-turbo',
+    temperature: 0.1,
+  });
+
+  const chain = loadQARefineChain(model);
+  const embeddings = new OpenAIEmbeddings();
+  const store = await MemoryVectorStore.fromDocuments(docs, embeddings);
+  const relevantDocs = await store.similaritySearch(question);
+
+  const res = await chain.invoke({
+    input_documents: relevantDocs,
+    question: question
+  })
+
+  return res.output_text;
 }
