d60d34601d30083319e06fce84eb718e3794138c
Styles <stylesk11@gmail.com>
2024-06-10 13:39:33 -0700

    add link to Insights

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,13 +5,28 @@ interface DashboardLayoutProps {
   children: React.ReactNode
 }
 
+const links = [
+  { href: '/', label: 'Home' },
+  { href: '/journal', label: 'Journal' },
+  { href: '/insights', label: 'Insights' },
+]
+
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
 
   return (
     <div className="w-screen h-screen relative">
-      <div className="absolute top-0 left-0 h-full w-[200px] border-r border-black/10">
-        Mood
-      </div>
+      <aside className="absolute top-0 left-0 h-full w-[200px] border-r border-black/10">
+        <div>
+          Mood
+        </div>
+        <ul>
+          {links.map((link, index) => (
+            <li key={index}>
+              <a href={link.href}>{link.label}</a>
+            </li>
+          ))}
+        </ul>
+      </aside>
       <div className="ml-[200px] h-full">
         <header className="h-[70px] border-b border-black/10">
           <div className="h-full w-full px-6 flex items-center justify-end">
