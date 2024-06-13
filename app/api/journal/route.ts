b8604d644ac9edd7395652c41764e1f791ee8201
Styles <stylesk11@gmail.com>
2024-06-13 14:29:49 -0700

    add user id to analysis creation

--- app/api/journal/route.ts
+++ app/api/journal/route.ts
@@ -18,6 +18,7 @@ export const POST = async () => {
 
   await prisma.analysis.create({
     data: {
+      userId: user.id,
       entryId: entry.id,
       mood: analysis.mood,
       summary: analysis.summary,
